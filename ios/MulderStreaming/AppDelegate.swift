a797facf4049afe2d2b9f182f03cd79f44c8a538
SoftApps.io <66966682+teamsoftapps@users.noreply.github.com>
2025-02-27 04:06:31 +0500

    4:06

--- ios/MulderStreaming/AppDelegate.swift
+++ ios/MulderStreaming/AppDelegate.swift
@@ -2,31 +2,32 @@ import UIKit
 import React
 import React_RCTAppDelegate
 
-import RNSplashScreen 
-
-
 @main
 class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+  override func application(
+    _ application: UIApplication, 
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+    
     self.moduleName = "MulderStreaming"
-   
 
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
+    // Initial props (if any)
     self.initialProps = [:]
- RNSplashScreen.show()
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    return result
   }
 
   override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
+    return self.bundleURL()
   }
 
   override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
+    #if DEBUG
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    #else
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    #endif
   }
 }
