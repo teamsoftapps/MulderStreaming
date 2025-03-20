dc9512483a58658e42cce59c71547b90b159955e
Development <66966682+teamsoftapps@users.noreply.github.com>
2025-03-20 09:53:53 -0700

    added context in mainactivity.kt

--- android/app/src/main/java/com/MulderStreaming/MainActivity.kt
+++ android/app/src/main/java/com/MulderStreaming/MainActivity.kt
@@ -1,29 +1,35 @@
 package com.MulderStreaming
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import android.os.Bundle
 import com.zoontek.rnbootsplash.RNBootSplash
+import com.google.android.gms.cast.framework.CastContext // Import this
 
 class MainActivity : ReactActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
-    super.onCreate(savedInstanceState) // super.onCreate(null) with react-native-screens
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Initialize the splash screen
+        RNBootSplash.init(this, R.style.BootTheme)
+        super.onCreate(savedInstanceState)
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "MulderStreaming"
+        // Initialize CastContext
+        val castContext = CastContext.getSharedInstance(this)
+        // Ensure cast context is initialized
+    }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "MulderStreaming"
+
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
